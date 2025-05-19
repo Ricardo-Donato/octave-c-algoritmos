6ab939abf1317388ac623b202c86870edf79864f
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-19 20:35:56 -0300

    Resolução do exercício 1

---
+++ aula2/atividades-19-05/num_intervalo_200_400.m
@@ -0,0 +1,7 @@
+num = input('Insira um número qualquer: ');
+
+if num >= 200 && num <= 400
+  fprintf('Este número está no intervalo de 200 e 400\n');
+else
+  fprintf('Este número não está no intervalo de 200 e 400\n');
+end
