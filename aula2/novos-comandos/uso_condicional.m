dcc6339995fd4484319548aa2701ff4b91422e8a
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-19 19:47:07 -0300

    Implementa o uso de condicionais ao octave

---
+++ aula2/novos-comandos/uso_condicional.m
@@ -0,0 +1,9 @@
+A = input('Informe um valor qualquer: ');
+B = input('Informe outro valor qualquer: ');
+soma = A + B;
+
+if soma > 10
+  fprintf('A soma entre os números inseridos é maior que 10, sendo igual a %d.\n', soma);
+else
+  fprintf('A soma entre os números inseridos é maior que 10, sendo igual a %d.\n', soma);
+end
