dacf3b77d1759b7ca5574fc03c7964d35c8d7dfa
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-19 19:34:48 -0300

    Correção dos exercícios no OCTAVE(GUI)

--- aula1/exercicios-12-05/calcula_consumo_km_por_litro.m
+++ aula1/exercicios-12-05/calcula_consumo_km_por_litro.m
@@ -4,4 +4,4 @@ consumo_litros = input('Ao fim, digite os litros de gasolina gastos nessa viagem
 km_percorridos = km_final - km_inicial;
 media_consumo = km_percorridos / consumo_litros;
 
-fprintf('Então, isso significa que a média de consumo do seu automóvel foi de %f km/l.\n', media_consumo);
+fprintf('Então, isso significa que a média de consumo do seu automóvel foi de %.2f km/l.\n', media_consumo);
