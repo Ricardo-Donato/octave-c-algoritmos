761ef3d5201f627a6cf08c21942a612ab30f9f7e
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-19 20:40:14 -0300

    Resolução do exercício 2

---
+++ aula2/atividades-19-05/soma_3_numeros.m
@@ -0,0 +1,6 @@
+num1 = input('Insira um número: ');
+num2 = input('Insira outro número: ');
+num3 = input('Insira mais um número: ');
+soma = num1 + num2 + num3;
+
+fprintf('A soma entre os valores digitados é igual a %d\n', soma);
