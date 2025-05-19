768e1f2dfe5213391de56619849018bbbed9c6c5
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-19 20:45:20 -0300

    Resolução do exercício 3

---
+++ aula2/atividades-19-05/soma_produto_variaveis.m
@@ -0,0 +1,9 @@
+num1 = input('Insira um número: ');
+num2 = input('Insira outro número: ');
+num3 = input('Insira mais um número: ');
+num4 = input('Insira mais outro número: ');
+soma = num1 + num2;
+produto = num3 * num4;
+produto_variaveis = soma * produto;
+
+fprintf('O produto entre a soma dos dois primeiros valores digitados e o produto dos dois últimos valores digitados é igual a %d\n', produto_variaveis);
