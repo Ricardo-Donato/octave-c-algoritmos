4ab4dbfd55ddfc16a70f3a20f6ca99879135d783
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-26 20:40:11 -0300

    Create while_octave.m

---
+++ aula3/novos-comandos/while_octave.m
@@ -0,0 +1,10 @@
+cont = 1;
+
+fprintf('Digite o %dº valor positivo: ',  cont);
+numero = input(' ');
+
+while numero > 0
+  cont++;
+  fprintf('Digite o %dº valor positivo: ',  cont);
+  numero = input(' ');
+end
