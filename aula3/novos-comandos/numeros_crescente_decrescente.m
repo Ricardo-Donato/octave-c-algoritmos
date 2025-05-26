0536ec367cb5063a666f7bc38adef13191e263df
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-26 20:09:30 -0300

    Create numeros_crescente_decrescente.m

---
+++ aula3/novos-comandos/numeros_crescente_decrescente.m
@@ -0,0 +1,11 @@
+fprintf('Números de 1 a 10 em ordem crescente:\n');
+
+for cont=1:10
+  fprintf('%d\n', cont);
+end
+
+fprintf('\nNúmeros de 1 a 10 em ordem decrescente:\n');
+
+for cont=10:-1:1
+  fprintf('%d\n', cont);
+end
