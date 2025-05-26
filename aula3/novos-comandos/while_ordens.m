1bbc87705b56e31cb190f6648536fa2b799fece8
Pedro Ricardo Donato da SIlva <155203759+Ricardo-Donato@users.noreply.github.com>
2025-05-26 20:40:16 -0300

    Create while_ordens.m

---
+++ aula3/novos-comandos/while_ordens.m
@@ -0,0 +1,14 @@
+numero = 1;
+
+while numero <= 10
+  fprintf('%d\n', numero);
+   numero++;
+end
+
+fprintf('\n\n');
+
+numero = 10;
+while numero >= 1
+  fprintf('%d\n', numero);
+  numero--;
+end
