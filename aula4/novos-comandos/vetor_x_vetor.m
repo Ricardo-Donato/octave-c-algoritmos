024772c423aac458be88bd474ce7e5a0d1911efa
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-03 13:57:52 -0300

    Create vetor_x_vetor.m

---
+++ aula4/novos-comandos/vetor_x_vetor.m
@@ -0,0 +1,26 @@
+%Crie um algoritmo que leia dois vetores de 5 posições e multiplique cada posição entre eles
+%Exiba eles armazenando em um novo vetor
+
+%Leia o vetor A
+disp("Digite o vetor A:");
+for i = 1:5
+  fprintf('\nA(%d): ', i);
+  vetA(i) = input(' ');
+end
+
+%Leia o vetor B
+disp("Digite o vetor B:");
+for i = 1:5
+  fprintf('\nB(%d): ', i);
+  vetB(i) = input(' ');
+end
+
+%Crie o vetor C
+for i = 1:5
+  vetC(i) = vetA(i)*vetB(i);
+end
+
+%Imprima na tela vetor C
+for i = 1:5
+  fprintf('\nO vetor resultante entre as posições %d e %d é %d', vetA(i), vetB(i), vetC(i));
+end
