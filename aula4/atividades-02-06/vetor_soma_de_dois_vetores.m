7c14044726555a0cca00446593575364eb1acb1b
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-04 10:51:51 -0300

    Atribui variável para loop for

--- aula4/atividades-02-06/vetor_soma_de_dois_vetores.m
+++ aula4/atividades-02-06/vetor_soma_de_dois_vetores.m
@@ -4,21 +4,21 @@
 disp('Preencha dois vetores de 10 números inteiros cada.');
 fprintf('\nDigite os valores do primeiro vetor: \n');
 
-for 1:10
+for i = 1:10
     vetA(i) = input(' ');
 end
 
 fprintf('\nDigite os valores do segundo vetor: \n');
-for 1:10
+for i = 1:10
     vetB(i) = input(' ');
 end
 
 %Armazenar
-for 1:10
+for i = 1:10
     vetC(i) = vetA(i) + vetB(i);
 end
 
 fprintf('\nA soma entre cada posição deles é: \n');
-for 1:10
+for i = 1:10
     fprintf('Soma da posição %d: %d', i, vetC(i));
 end
