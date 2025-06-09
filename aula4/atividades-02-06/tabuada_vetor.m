0566bf6e4e284fb2cf656658bbbdc6bf71a3f28b
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-09 08:21:53 -0300

    Corrige codificação do exercício 3

--- aula4/atividades-02-06/tabuada_vetor.m
+++ aula4/atividades-02-06/tabuada_vetor.m
@@ -1,13 +1,13 @@
-%Monte um algoritmo que, dado um n�mero inteiro, armazene-o
-% e mostre sua tabuada dentro de um vetor de 10 posi��es;
+%Monte um algoritmo que, dado um número inteiro, armazene-o
+% e mostre sua tabuada dentro de um vetor de 10 posições;
 
-x = input('Informe um n�mero inteiro:');
+x = input('Informe um número inteiro:');
 
 for i = 1:10
     vetA(i) = x*i;
 end
 
-fprintf('A tabuada do n�mero %d �: ', x);
+fprintf('A tabuada do número %d é: ', x);
 for i = 1:10
     fprintf('%d x %d = %d\n', x, i, vetA(i));
 end
