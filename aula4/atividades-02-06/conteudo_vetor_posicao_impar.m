da6ada0b6aff0564c134357dfa76271cdde54eac
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-09 08:10:29 -0300

    Corrige codificação do exercício 1

--- aula4/atividades-02-06/conteudo_vetor_posicao_impar.m
+++ aula4/atividades-02-06/conteudo_vetor_posicao_impar.m
@@ -1,15 +1,15 @@
-%Crie um programa que leia um vetor de 10 nÃºmeros
-%Depois exiba o conteÃºdo dos vetores de Ã­ndice Ã­mpares;
+%Crie um programa que leia um vetor de 10 números
+%Depois exiba o conteúdo dos vetores de índice ímpares;
 
 %Ler um vetor
 for i = 1:10
-  fprintf('\Digite um valor para a posiÃ§Ã£o %d do vetor A:\n', i);
+  fprintf('\Digite um valor para a posição %d do vetor A:\n', i);
   vetA(i) = input(' ');
 end
 
-%Exibir apenas posiÃ§Ãµes Ã­mpares usando mod
+%Exibir apenas posições ímpares usando mod
 for i = 1:10
   if mod(i, 2) ~= 0
-    fprintf('Valor da posiï¿½ï¿½o %d do vetor A: %d\n', i, vetA(i));
+    fprintf('Valor da posição %d do vetor A: %d\n', i, vetA(i));
   end
 end
