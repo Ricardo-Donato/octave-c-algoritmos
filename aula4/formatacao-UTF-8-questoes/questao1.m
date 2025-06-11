c8c3968c402cdcbb2d1a96c17fe4566e1c74315a
Ricardo-Donato <pdonatodasilva@gmail.com>
2025-06-11 19:40:02 -0300

    Update questao1.m

--- aula4/formatacao-UTF-8-questoes/questao1.m
+++ aula4/formatacao-UTF-8-questoes/questao1.m
@@ -1,15 +1,20 @@
-%Crie um programa que leia um vetor de 10 números
-%Depois exiba o conteúdo dos vetores de índice ímpares;
+%Crie um programa que leia um vetor de 10 numeros
+%Depois exiba o conteudo dos vetores de indice impares;
 
 %Ler um vetor
+disp('Preencha o vetor de 10 posições abaixo:')
 for i = 1:10
-  fprintf('\Digite um valor para a posição %d do vetor A:\n', i);
+  fprintf('\n');
   vetA(i) = input(' ');
 end
 
-%Exibir apenas posições ímpares usando mod
+%Exibir apenas posições impares usando mod
+fprintf('\n');
+disp('Exibir apenas posições impares:');
 for i = 1:10
   if mod(i, 2) ~= 0
-    fprintf('Valor da posição %d do vetor A: %d\n', i, vetA(i));
+    fprintf('\n');
+    fprintf('\n Valor da posição %d do vetor A: %d', i, vetA(i));
   end
 end
+fprintf('\n');
