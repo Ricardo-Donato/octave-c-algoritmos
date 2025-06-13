6fee9eba61fddca0753079464d24b58ad7c34c52
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-13 17:17:24 -0300

    Adiciona a resolução do primeiro exercício do trabalho final de octave

---
+++ aula6/trabalho-em-octave/exercicio_soma_de_vetores.m
@@ -0,0 +1,13 @@
+disp('Exercício 1: Soma de Vetores');
+disp('Em um jogo de nave espacial, sua nave se move para v1=(5,2) e depois v2=(-3,4).');
+disp('Vamos calcular a posição final somando os vetores.');
+disp('------------------------------------------------------');
+
+v1 = [5 2];
+v2 = [-3 4];
+
+posicao_final = v1 + v2;
+
+fprintf('O vetor do primeiro movimento é v1 = (%d, %d)\n', v1(1), v1(2));
+fprintf('O vetor do segundo movimento é v2 = (%d, %d)\n', v2(1), v2(2));
+fprintf('A posição final da nave (v1 + v2) é: (%d, %d)\n', posicao_final(1), posicao_final(2));
