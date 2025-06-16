78d2986ddbfce7e7d8ae1d4553d2a1f8271e3376
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-16 15:27:19 -0300

    Resolução da segunda questão do trabalho final de octave

---
+++ aula5/trabalho-final-em-octave/multiplicacao_escalar_vetor.m
@@ -0,0 +1,13 @@
+disp('Exercício 2: Multiplicação de um Escalar por um Vetor');
+disp('Você está organizando uma corrida de carrinhos de brinquedo.');
+disp('O vetor v = (3,2) representa a velocidade do carrinho nas direções x e y (em metros por segundo).');
+disp('Porém, depois de um pequeno impulso, a velocidade do carrinho aumenta 3 vezes.');
+disp('Vamos calcular a nova velocidade do carrinho após o aumento.');
+disp('------------------------------------------------------');
+
+v = [3 2];
+
+nova_velocidade = v * 3;
+
+fprintf('A velocidade original era (%d, %d)\n', v(1), v(2));
+fprintf('Com o pequeno impulso, a velocidade final do carrinho (v * 3) é: (%d, %d)\n', nova_velocidade(1), nova_velocidade(2));
