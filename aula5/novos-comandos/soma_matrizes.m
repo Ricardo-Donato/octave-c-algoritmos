5d0980658086a2806de9547dd2d9784f4c496f2f
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-17 16:18:55 -0300

    Adiciona quebras de linha para melhorar a legibilidade do código

--- aula5/novos-comandos/soma_matrizes.m
+++ aula5/novos-comandos/soma_matrizes.m
@@ -1,4 +1,5 @@
 %Leia 2 matrizes (2x4) e realize a soma
+
 %Le Matriz
 disp('Matriz A');
 for I=1:2 %Linhas
@@ -8,6 +9,7 @@ for I=1:2 %Linhas
      MatA(I,J)=input(' ');
   end
 end
+
 %Le Matriz
 disp('Matriz B');
 for I=1:2 %Linhas
@@ -17,12 +19,14 @@ for I=1:2 %Linhas
      MatB(I,J)=input(' ');
   end
 end
+
 %Soma das Matrizes
 for I=1:2
   for J=1:4
     MatC(I,J)=MatA(I,J)+MatB(I,J);
   end
 end
+
 %Mostra Matriz
 disp('Matriz A Digitada');
 for I=1:2 %Linhas
@@ -31,6 +35,7 @@ for I=1:2 %Linhas
      fprintf('\t%d',MatA(I,J));
    end
 end
+
 %Mostra Matriz
 disp('Matriz B Digitada');
 for I=1:2 %Linhas
@@ -39,6 +44,7 @@ for I=1:2 %Linhas
      fprintf('\t%d',MatB(I,J));
    end
 end
+
 %Mostra Matriz
 disp('Matriz C Resposta');
 for I=1:2 %Linhas
@@ -47,10 +53,3 @@ for I=1:2 %Linhas
      fprintf('\t%d',MatC(I,J));
    end
 end
-
-
-
-
-
-
-
