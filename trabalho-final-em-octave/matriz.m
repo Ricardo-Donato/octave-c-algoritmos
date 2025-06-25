2e4d0bf4792fc05c0974653021baf550610a2185
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-25 15:24:52 -0300

    Remove comentários e saídas de execução desnecessárias do código

--- trabalho-final-em-octave/matriz.m
+++ trabalho-final-em-octave/matriz.m
@@ -16,31 +16,3 @@ for i = 1:length(palavraOriginal)
     caractere = palavraOriginal(i);
     fprintf('Letra: %s - ASCII correspondente: %d\n', caractere, double(caractere));
 end
-
-%matrizCifradora =
-%
-%   12    4    1
-%    3    1   -2
-%    1    0    2
-%
-%ans = -9
-%matrizDecifradora =
-%
-%  -0.2222   0.8889   1.0000
-%   0.8889  -2.5556  -3.0000
-%   0.1111  -0.4444        0
-%
-%Digite uma palavra (min. 5, max. 9 caracteres): 
-%Por favor, digite novamente (min. 5, max. 9 caracteres): 
-%Por favor, digite novamente (min. 5, max. 9 caracteres): 
-%Por favor, digite novamente (min. 5, max. 9 caracteres): 
-%Palavra original digitada: Pedro
-%
-%Caracteres percorridos + valores em ASCII:
-%Letra: P - ASCII correspondente: 80
-%Letra: e - ASCII correspondente: 101
-%Letra: d - ASCII correspondente: 100
-%Letra: r - ASCII correspondente: 114
-%Letra: o - ASCII correspondente: 111
-%
-%[Execution complete with exit code 0]
