8a7abed2664aabd1a7228d150c52fafa3e02bdbd
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-25 14:50:55 -0300

    Refatora validação de entrada para palavra original, eliminando variável intermediária e utilizando diretamente a função length

--- trabalho-final-em-octave/matriz.m
+++ trabalho-final-em-octave/matriz.m
@@ -3,18 +3,16 @@ det(matrizCifradora)
 matrizDecifradora = inv(matrizCifradora)
 
 palavraOriginal = input('Digite uma palavra (min. 5, max. 9 caracteres): ', 's');
-qtdCaracteres = length(palavraOriginal);
 
-while (qtdCaracteres < 5 || qtdCaracteres > 9)
+while (length(palavraOriginal) < 5 || length(palavraOriginal) > 9)
     fprintf('\n');
     palavraOriginal = input('Por favor, digite novamente (min. 5, max. 9 caracteres): ', 's');
-    qtdCaracteres = length(palavraOriginal);
 end
 
 fprintf('\nPalavra original digitada: %s\n', palavraOriginal);
 fprintf('\nCaracteres percorridos + valores em ASCII:\n');
 
-for i = 1:qtdCaracteres
+for i = 1:length(palavraOriginal)
     caractere = palavraOriginal(i);
     fprintf('Letra: %s - ASCII correspondente: %d\n', caractere, double(caractere));
 end
