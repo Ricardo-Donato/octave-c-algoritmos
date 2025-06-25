a349d2577480cc3583041be8fba7a93521232a95
Pedro Ricardo Donato da SIlva <pdonatodasilva@gmail.com>
2025-06-25 11:07:48 -0300

    2.1. Solicitação e Validação da Palavra Original

--- trabalho-final-em-octave/matriz.m
+++ trabalho-final-em-octave/matriz.m
@@ -1,3 +1,13 @@
 matrizCifradora = [12 4 1; 3 1 -2; 1 0 2]
 det(matrizCifradora)
 matrizDecifradora = inv(matrizCifradora)
+
+palavraOriginal = input('Digite uma palavra (min. 5, max. 9 caracteres): ', 's');
+qtdCaracteres = length(palavraOriginal);
+
+while (qtdCaracteres < 5 || qtdCaracteres > 9)
+    fprintf('\n');
+    palavraOriginal = input('Por favor, digite novamente (min. 5, max. 9 caracteres): ', 's');
+end
+
+fprintf('\nPalavra original digitada: %s\n', palavraOriginal);
